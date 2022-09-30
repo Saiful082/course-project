db0d733ed73993174a5e3850c9a1977f3e38eec2
Saiful082 <smsaifulislam082@gmail.com>
2022-09-30 10:32:01 +0600

    header sesponsive

--- main.js
+++ main.js
@@ -33,7 +33,15 @@ const closeBtn = document.querySelector("#close-menu-btn");
 
 
 menuBtn.addEventListener('click', () => {
-    menu.getElementsByClassName.display = "flex";
-    closeBtn.getElementsByClassName.display = "inline-block";
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
     menuBtn.style.display = "none";
 })
+
+// close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click', closeNav)
